4ef983213ee0bca0ee3e66f4e427e3007f94b9fe
v3rb4 <verba@keemail.me>
2025-06-27 10:14:24 +0300

    Add routes for Posts and ContactRequests and basic views

---
+++ app/controllers/contact_requests_controller.rb
@@ -0,0 +1,7 @@
+class ContactRequestsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'pages/home'
-  root 'pages#home'
+
+  # Public routes
+  root 'pages#show', slug: 'home'
+
+  get '/services', to: 'pages#show', defaults: { slug: 'services' }
+  get '/blog', to: 'posts#index'
+  get '/contact', to: 'contact_requests#new'
+  post '/contact', to: 'contact_requests#create'
+
+  # Optional: Health check route (если хочешь оставить)
   get 'up' => 'rails/health#show', as: :rails_health_check
-  get 'contacts', to: 'pages#contacts'
 end
