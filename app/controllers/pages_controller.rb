929cd30eb9b02030d99e0c293c93fa2761e51609
v3rb4 <verba@keemail.me>
2025-06-28 19:54:40 +0300

    Implement PagesController#show to render pages by slug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def home; end
+  def show
+    @page = Page.find_by(slug: params[:slug], published: true)
+    if @page
+      render :show
+    else
+      render file: "#{Rails.root}/public/404.html", status: :not_found
+    end
+  end
 end
