879a9c8b7b43d61a6507e81faa761618ebd46cfc
v3rb4 <verba@keemail.me>
2025-06-29 20:28:32 +0300

    Implement ContactRequestsController with contact form and submission handling

--- app/controllers/contact_requests_controller.rb
+++ app/controllers/contact_requests_controller.rb
@@ -1,7 +1,20 @@
 class ContactRequestsController < ApplicationController
   def new
+    @contact_request = ContactRequest.new
   end
 
   def create
+    @contact_request = ContactRequest.new(contact_request_params)
+    if @contact_request.save
+      redirect_to contact_path, notice: 'Thank you for your message!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_request_params
+    params.require(:contact_request).permit(:name, :email, :message)
   end
 end
